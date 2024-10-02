e0516d2d06011cda2c3e85f8ad36dea992b71983
Harry William Foster <harry.foster-2@student.manchester.ac.uk>
2024-10-02 14:28:46 +0100

    Replaced the multiply with logical shifting and removed the branching

--- solution_stretch.s
+++ solution_stretch.s
@@ -2,13 +2,7 @@
     
         beq x10, x0, end
         li x5, 1
-        li x11, 2
-
-loop
-        mul x5, x5, x11
-        subi x10, x10, 1
-        bne x10, x0, loop
-
+        sll x5, x5, x10
 end
         sw x5, res, x6
         ecall
