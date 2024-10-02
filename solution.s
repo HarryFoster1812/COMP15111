3a6498f909e884a28eddc23b93d3479efdd5ef4f
Harry William Foster <harry.foster-2@student.manchester.ac.uk>
2024-10-02 13:51:11 +0100

    removed any reference to val0 and replaced it with x0, removed unit and replaced the sub to subi, combined the beq and jal instructions

--- solution.s
+++ solution.s
@@ -1,24 +1,19 @@
         lw x10, cnt
-        lw x11, unit
         lw x12, alpha
-        lw x13, val0
     
-        beq x10, x13, end
+        beq x10, x0, end
         mv x5, x11
 
 loop
         mul x5, x5, x12
         sw x5, res, x6
-        sub x10, x10, x11
-        beq x10, x13, end
-        jal x0, loop
+        subi x10, x10, 1
+        bne x10, x0, loop
 
 end
         li x17, 5
         ecall
     
-val0    defw    0
-unit    defw    1
 alpha   defw    2
 cnt     defw    10
 res     defw    1
