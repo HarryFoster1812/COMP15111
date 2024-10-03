d958a6567c7e11dfb22d2b590aec7f0f9ab86c0d
Pavlos Petoumenos <pavlos.petoumenos@manchester.ac.uk>
2024-10-03 14:48:22 +0100

    Initial commit

---
+++ part1.s
@@ -0,0 +1,43 @@
+	j main
+
+op1_msg	defb	"Operand 1:  \0"
+op2_msg	defb	"Operand 2:  \0"
+
+op1	defb 0
+op2 	defb 0
+res	defb 0
+
+	align
+main
+	la x10, op1_msg 
+	li x17, 2
+	ecall		; print("Operand 1: ")
+	
+	; while True:
+	;   read character into x10
+	;   break if x10 is a decimal digit
+	;
+	; store in op1 the number corresponding to the character x10
+	; print character in x10
+	; print '\n' (change line)
+	
+	la x10, op2_msg
+	li x17, 2
+	ecall 		; print("Operand 2: ")
+
+	; while True:
+	;   read character into x10
+	;   break if x10 is a decimal digit
+	;
+	; store in op1 the number corresponding to the character x10
+	; print character in x10
+	; print '\n' (change line)
+	
+	; res = op1 + op2
+	; print("Result of Addition: ")
+	; print(res) on the same line as the previous print
+	; print '\n' (change line)
+    
+	li x17, 5
+	ecall 		; stop!
+	
